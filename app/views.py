66062e57b9b687e8df145c98d37f717d8dc7df16
Polkis <piterek190405@wp.pl>
2025-03-31 16:38:48 +0200

    Rafik push

--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
-from pickletools import read_decimalnl_long
-
+import json
 from bson import ObjectId
 from flask import render_template, Blueprint, request, url_for, redirect, session
 from app import db, auth
@@ -58,20 +57,60 @@ def EditSpecs(*, context):
 
 @app_blueprint.route("/listujModele", methods = ['POST', 'GET'])
 @auth.login_required()
-def listModels(*, context):
-    allModels = {"test": "test"}
+def ListModels(*, context):
+
+    if not db.models.find_one():
+        allModels = False
+    else:
+        allModels = list(db.models.find())
+
+        for model in allModels:
+            model["names"] = ""
+            listLength = len(model["specs"]) - 1
+            index = 0
+            for spec in model["specs"]:
+                if index != listLength:
+                    model["names"] += spec["name"] + ", "
+                    index += 1
+                else:
+                    model["names"] += spec["name"]
+
+
 
     if request.method == "POST":
         action = request.form.get("action")
 
+        if action == "delete":
+            deleteID = request.form.get("deleteModel")
+            db.models.find_one_and_delete({"_id": ObjectId(deleteID)})
+            return redirect(url_for("app.ListModels"))
+
         if action == "add":
-            return redirect(url_for("app.addModels"))
+            return redirect(url_for("app.AddModels"))
 
     return render_template("listModels.html", models = allModels ,username = context['user']['name'])
 
 @app_blueprint.route("/dodajModel", methods = ['POST', 'GET'])
 @auth.login_required()
-def addModels(*, context):
+def AddModels(*, context):
+
     allTypes = db.specs.find_one({"name": "Typ urządzenia"})
-    allModels = {"test": "test"}
-    return render_template("editModels.html", types = allTypes["options"] ,models = allModels ,username = context['user']['name'])
+    allManufacturers = db.specs.find_one({"name": "Producent"})
+    allSpecs = list(db.specs.find({"name": {"$nin": ["Typ urządzenia", "Producent"]}}))
+
+    if not allTypes:
+        typeList = False
+    else:
+        typeList = allTypes["options"]
+
+    if not allManufacturers:
+        manufacturerList = False
+    else:
+        manufacturerList = allManufacturers["options"]
+
+    if request.method == "POST":
+        data = json.loads(request.form['data'])
+        db.models.insert_one(data)
+        return redirect(url_for("app.ListModels"))
+
+    return render_template("editModels.html", types = typeList, manufacturers = manufacturerList, specs = allSpecs, username = context['user']['name'])
